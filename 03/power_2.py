6c4530bdbbe394919ff1d814bfe43d87a6546a64
bearHanded <bcm3223@gmail.com>
2021-12-03 02:36:06 -0500

    3

--- 03/power_2.py
+++ 03/power_2.py
@@ -2,8 +2,6 @@ from util import christmas_input
 
 FILE = './input.txt'
 
-rows = christmas_input.file_to_array(FILE)
-
 
 def map_sums(active_rows):
     rotated = list(zip(*active_rows[::-1]))
@@ -16,8 +14,6 @@ def map_sums(active_rows):
         sums.append({0: 0, 1: 0})
         for bit in bits:
             sums[row_idx][bit] += 1
-
-            # get max value from dict
         v = list(sums[row_idx].values())
         k = list(sums[row_idx].keys())
         most_common.append(k[v.index(max(v))])
@@ -31,34 +27,27 @@ def to_value(arr):
 
 
 def oxygen_filter(o_row, idx, curr_sums, curr_gamma):
-    most_common_bit = curr_gamma[idx]
-    if curr_sums[idx][0] == curr_sums[idx][1]:
-        most_common_bit = 1
+    most_common_bit = curr_gamma[idx] if curr_sums[idx][0] != curr_sums[idx][1] else 1
     return o_row[idx] == str(most_common_bit)
 
 
 def carbon_filter(c_row, idx, curr_sums, curr_epsilon):
-    least_common_bit = curr_epsilon[idx]
-    if curr_sums[idx][0] == curr_sums[idx][1]:
-        least_common_bit = 0
+    least_common_bit = curr_epsilon[idx] if curr_sums[idx][0] != curr_sums[idx][1] else 0
     return c_row[idx] == str(least_common_bit)
 
 
-myArr, gamma, epsilon = map_sums(rows)
+rows = christmas_input.file_to_array(FILE)
 
-###
 # POWER!!!
-##
+myArr, gamma, epsilon = map_sums(rows)
 g = to_value(gamma)
 e = to_value(epsilon)
-print("Power: ")
-print(g, "*", e, ":", g * e)
+print("Power: ", g, "*", e, ":", g * e)
 
-# oxygen gen rating
+# Oxygen
 gamma_idx = 0
 oxygen = rows.copy()
 
-# for most_common_bit in gamma:
 while len(oxygen) > 1:
     current_sums, current_gamma, current_epsilon = map_sums(oxygen)
     oxygen = [x for x in oxygen if oxygen_filter(x, gamma_idx, current_sums, current_gamma)]
@@ -77,5 +66,4 @@ print(carbon)
 
 c = to_value(carbon)
 o = to_value(oxygen)
-print("Part 2: ")
-print(o, "*", c, ":", o * c)
+print("Part 2: ", o, "*", c, ":", o * c)
