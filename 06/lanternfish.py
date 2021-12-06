6db22898b0163d050f3d3d1f018986265f7f5039
bearHanded <bcm3223@gmail.com>
2021-12-06 01:31:52 -0500

    smarter fish

--- 06/lanternfish.py
+++ 06/lanternfish.py
@@ -1,80 +1,23 @@
-from functools import reduce
-
 from util import christmas_input
-import datetime
 from collections import Counter
 
 FILE = './input.txt'
 TOTAL_DAYS = 256  # 18 in example, 80 part one, 256 part two
-DETAILED_PRINT = False
 
 
-# Original Part 1 Solution
-def go_fish(fish_state, days):
-    print("Initial state:", fish_state)
-    for day in range(0, days):
-        queued_fish = 0
-        for i in range(len(fish_state)):
-            # Tick Everything
-            fish_state[i] = fish_state[i] - 1
-            # Move -1 up to new number and add one
-            if fish_state[i] < 0:
-                fish_state[i] = 6
-                queued_fish += 1
-        fish_state.extend([8] * queued_fish)
-
-        if DETAILED_PRINT:
-            print("After", day + 1, "days:", fish_state)
-        elif day % 5 == 0:
-            print("Day", day, "-", datetime.datetime.now())
-    print("Total Fish:", len(fish_state))
-    return fish_state
-
-
-class FishFamily:
-    def __init__(self, timer, count):
-        self.timer = timer
-        self.count = count
-
-    def countdown(self):
-        self.timer -= 1
-        return self.timer
-
-
-    def add(self, count):
-        self.count += count
-        return self
-
-
-def smart_fish(initial_state, days):
+def go_fish(initial_state, days):
     counts = Counter(initial_state)
-    fish_state = []
+    fish_state = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     for key in counts.keys():
-        fish_state.append(FishFamily(key, counts[key]))
+        fish_state[key] = counts[key]
 
     for day in range(0, days):
-        queued_fish = 0
-        removed_fish = None
-        for family in fish_state:
-            new_timer = family.countdown()
-            if new_timer < 0:
-                queued_fish += family.count
-                removed_fish = family
-
-        if queued_fish:
-            # A family popped. Add them to any existing 6, add children at 8, remove -1
-            joined_idx = [idx for idx, element in enumerate(fish_state) if element.timer == 6]
-            if len(joined_idx) > 0:
-                fish_state[joined_idx[0]].add(queued_fish)
-            else:
-                fish_state.append(FishFamily(6, queued_fish))
-            fish_state.append(FishFamily(8, queued_fish))
-            if removed_fish is not None:
-                fish_state.remove(removed_fish)
-
-        if day % 5 == 0:
-            print("Day", day, "-", datetime.datetime.now())
-    print("Total Fish:", reduce(lambda x, y: x.add(y.count), fish_state).count)
+        popped = fish_state.pop(0)
+        fish_state.append(0)
+        if popped > 0:
+            fish_state[6] += popped
+            fish_state[8] += popped
+    print("Total Fish:", sum(fish_state))
     return fish_state
 
 
