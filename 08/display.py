11d8a145b7047b609a7a343967e3b96cdeabbd3b
bearHanded <bcm3223@gmail.com>
2021-12-09 00:56:58 -0500

    day 9, pt 1

--- 08/display.py
+++ 08/display.py
@@ -1,21 +1,73 @@
 from util import christmas_input
 
-FILE = './test.txt'
-
-char_lookup = {
+# FILE = './test_input.txt'
+FILE = 'input.txt'
+COUNT_LOOKUP = {
     2: 1,
     4: 4,
     3: 7,
     7: 8
 }
 
-# Parse
-rows = [row.split("|") for row in christmas_input.file_to_array(FILE)]
-num_count = 0
-for codes in rows:
-    output = codes[1].split(" ")
-    for display in output[1]:
-        if char_lookup[len(display)] in [1, 4, 7, 8]:
-            num_count += 1
 
-print(num_count)
+def sort_display(display_string):
+    return "".join(sorted(display_string))
+
+
+# Part 1
+def solve_wires(rows):
+    all_output = []
+    for codes in rows: # The rules change per-row
+        wire_map = {}
+        row_wires = codes[0]
+
+        for combination in row_wires:  # Easy lookups: 1, 4, 7, 8
+            digit = COUNT_LOOKUP.get(len(combination), None)
+            if digit is not None:
+                wire_map[digit] = combination
+
+        while len(wire_map) < 10:
+            for combination in row_wires:
+                if len(combination) == 6:  # 0, 6
+                    if all(x in combination for x in wire_map[4]):
+                        wire_map[9] = combination
+                    elif all(x in combination for x in wire_map[1]):
+                        wire_map[0] = combination
+                    else:
+                        wire_map[6] = combination
+                elif len(combination) == 5:  # 2, 3, 5, 9
+                    if all(x in combination for x in wire_map[1]):
+                        wire_map[3] = combination
+                    elif all(x in ''.join(set(combination + wire_map[4])) for x in wire_map[8]):
+                        # unique wires in 4 + 2 = 8
+                        wire_map[2] = combination
+                    elif len(wire_map) == 9:
+                        wire_map[5] = combination
+
+        # list out keys and values separately
+        sorted_displays = {sort_display(v): k for k, v in wire_map.items()}
+
+        display = []
+        for combination in [sort_display(x) for x in codes[1]]:
+            display.append(sorted_displays[combination])
+        all_output.append(int("".join([str(i) for i in display])))  # join up all the numbers as if they were strings
+    print("Sum total:   ", sum(all_output))
+
+
+# Part 2
+def count_easy(rows):
+    num_count = 0
+    for codes in rows:  # The rules change per-row
+        wire_map = {}
+        output = codes[1]
+        for display in output:
+            digit = COUNT_LOOKUP.get(len(display), None)
+            if digit is not None:
+                num_count += 1
+    print("Known values:", num_count)
+
+
+# Parse
+rows = [[segment.strip().split(" ") for segment in row.split("|")] for row in christmas_input.file_to_array(FILE)]
+count_easy(rows)
+solve_wires(rows)
--- 09/topography.py
+++ 09/topography.py
@@ -1,27 +1,32 @@
 from util import christmas_input
 
-FILE = './input.txt'
-
-
-def align_crabs(crabs):
-    costs = []
-    for position in range(0, max(crabs) + 1):
-        costs.append(sum(abs(position - x) for x in crabs))
-    print("Part 1:", min(costs))
-
-
-def scaling_crabs(crabs):
-    costs = []
-    for position in range(0, max(crabs) + 1):
-        costs.append(sum(fuel_burn(position, x) for x in crabs))
-    print("Part 2:", min(costs))
-
-
-def fuel_burn(position, x):
-    return sum([i for i in range(1, abs(position - x) + 1)])
-
+FILE = './test_input.txt'
 
 # Parse
-values = [int(x) for x in christmas_input.file_as_string(FILE).split(",")]
-align_crabs(values)
-scaling_crabs(values)
+rows = christmas_input.file_to_array(FILE)
+parsed = [[int(value) for value in row] for row in rows]
+
+
+def explore_basin(depths, coordinate):
+    # recursive search in basin range
+    return 0
+
+
+danger = []
+basins = {}
+for y in range(0, len(parsed)):
+    for x in range(0, len(parsed[0])):
+        depth = parsed[y][x]
+        if (y == 0 or parsed[y - 1][x] > depth) and \
+                (y + 1 >= len(parsed) or parsed[y + 1][x] > depth) and \
+                (x == 0 or parsed[y][x - 1] > depth) and \
+                (x + 1 >= len(parsed[0]) or parsed[y][x + 1] > depth):
+            danger.append((parsed[y][x] + 1))
+            basins[(x, y)] = parsed[y][x]
+
+sizes = 0
+for basin in basins.keys():
+    sizes += explore_basin(parsed, basin)
+
+print(basins)
+print("Part 1:", sum(danger))
